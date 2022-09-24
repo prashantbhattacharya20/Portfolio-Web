4f2577600cc085c6d9658ed1b77973e6f249c79b
Prashant Bhattacharya <prashantbhattacharya72@gmail.com>
2022-09-25 02:28:43 +0530

    Created SideMenu

---
+++ lib/screens/main/components/coding.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/animated_progress_indicator.dart';
+import '../../../constants.dart';
+
+class Coding extends StatelessWidget {
+  const Coding({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            "Coding",
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ),
+        AnimatedLinearProgressIndicator(percentage: 0.65, label: "Java"),
+        AnimatedLinearProgressIndicator(percentage: 0.85, label: "Dart"),
+        AnimatedLinearProgressIndicator(percentage: 0.55, label: "Flutter"),
+        AnimatedLinearProgressIndicator(percentage: 0.80, label: "MySQL"),
+        AnimatedLinearProgressIndicator(percentage: 0.70, label: "HTML"),
+      ],
+    );
+  }
+}
---
+++ lib/screens/main/components/side_menu.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portfolio_web/constants.dart';
+import 'package:portfolio_web/screens/main/components/skills.dart';
+import '../../../components/animated_progress_indicator.dart';
+import 'area_info_text.dart';
+import 'coding.dart';
+import 'knowledges.dart';
+import 'my_info.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          MyInfo(),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(defaultPadding),
+              child: Column(
+                children: [
+                  AreaInfoText(
+                    title: "Residence",
+                    text: "India",
+                  ),
+                  AreaInfoText(
+                    title: "City",
+                    text: "Bhilai, Chhattisgarh",
+                  ),
+                  AreaInfoText(
+                    title: "Age",
+                    text: "21",
+                  ),
+                  Skills(),
+                  SizedBox(height: defaultPadding),
+                  Coding(),
+                  Knowledges(),
+                  Divider(),
+                  SizedBox(height: defaultPadding / 2),
+                  TextButton(
+                    onPressed: () {}, 
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          Text(
+                            "DOWNLOAD CV",
+                            style: TextStyle(
+                              color: Theme.of(context).textTheme.bodyText1!.color,
+                            ),
+                          ),
+                          SizedBox(width: defaultPadding / 2),
+                          SvgPicture.asset("assets/icons/download.svg")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: defaultPadding),
+                    color: Color(0xFF24242E),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        IconButton(onPressed: () {}, icon: SvgPicture.asset("assets/icons/linkedin.svg")),
+                        IconButton(onPressed: () {}, icon: SvgPicture.asset("assets/icons/github.svg")),
+                        IconButton(onPressed: () {}, icon: SvgPicture.asset("assets/icons/twitter.svg")),
+                        Spacer(),
+                      ]
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
